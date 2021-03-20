2b08513142d8d738041a6a062d479296819c9579
corazonnn <zero7364@icloud.com>
2021-03-20 16:10:21 +0900

    [add]twitter botがツイートする
    ツイートボタンを押した時、userに紐づくtweetテーブルの最新の情報がTwitter上にツイートされる。

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,19 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :sessions, only: :create 
 
+
+  get   '/login',  to: 'top#index'
+  get   '/logout', to: 'top#index'
+  get   '/signup', to: 'top#index'
+  resources :tweets, only: [:new, :create] do
+    collection do
+      get 'post'
+      get 'congrats'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      resources :sessions, only: :create
       resources :user, only: :create
       delete :logout, to: "sessions#logout"
       get :logged_in, to: "sessions#logged_in"
