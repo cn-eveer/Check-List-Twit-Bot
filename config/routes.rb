02f424525f019a9ca1c081f70b779f5617c94117
Sien <sienreeve46@gmail.com>
2021-03-20 14:30:04 +0900

    add app and routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,25 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :sessions, only: :create
+  
+  # Authentication
+  get '/login',  to: 'top#index'
+  get '/logout', to: 'top#index'
+  get '/signup', to: 'top#index'
+
+  # Checklist
+  get '/checklist', to: 'top#index'
+  get '/checklist/new', to: 'top#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :sessions, only: :create 
+
       resources :user, only: :create
       delete :logout, to: "sessions#logout"
       get :logged_in, to: "sessions#logged_in"
+
+      resources :checklists, only: [ :index, :create ]
     end
   end
 end
