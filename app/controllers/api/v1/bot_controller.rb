bb4451a47163bc315273e49053a47fe78ad73975
cn-eveer <64426194+cn-eveer@users.noreply.github.com>
2021-03-24 20:37:41 +0900

    Update bot_controller.rb
    remove twitter api key

--- app/controllers/api/v1/bot_controller.rb
+++ app/controllers/api/v1/bot_controller.rb
@@ -34,10 +34,10 @@ class Api::V1::BotController < ApplicationController
 
   def set_bot
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key = "tsPRWGyiTjlg3HpXXU8YuN2CG"
-      config.consumer_secret = "f1DLWH8TWoMjr81CsrxTipAvapJ3LCRW3y2cYa1vZu9civYOTW"
-      config.access_token = "1372173320951529474-gBoUEnM3F2qFKV5iEk3PC0GkREerYm"
-      config.access_token_secret = "tv3ECL6m89Yp8P0P6PbGlwcqJTvyj4rEQEoKMO0ck8TCs"
+      config.consumer_key = 
+      config.consumer_secret = 
+      config.access_token = 
+      config.access_token_secret = 
     end
   end
 end
